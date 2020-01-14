f133e250eea254fa979b22ff52a715608de31d11
Naomi Rodriguez <naomi.rodriguez7@aol.com>
2020-01-14 14:07:26 -0500

    added mouseeneter in line 9

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+
+
+
+//mouseenter
+const busImage = document.querySelector("img")
+console.log(busImage)
+
+busImage.addEventListener("mouseenter", () => {
+  busImage.style.transform = "scale(1.2)";
+  busImage.style.transition = "transform 0.3s"
+});
+busImage.addEventListener("mouseleave", () => {
+    busImage.style.transform = "scale(1)"
+  });
+
+  
