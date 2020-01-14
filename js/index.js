7d1d186791345171fed90780427fa0bc4508d129
Naomi Rodriguez <naomi.rodriguez7@aol.com>
2020-01-14 14:11:27 -0500

    added stopProgation in line 61

--- js/index.js
+++ js/index.js
@@ -26,3 +26,39 @@ const btn = document.querySelector('.btn');
 btn.addEventListener('click', event => {
   btn.innerHTML = `Click count: ${event.detail}`;
 });
+
+
+const footColor = document.querySelector('footer');
+
+console.log(footColor);
+footColor.addEventListener('dblclick', (event) => {
+    console.log(footColor);
+    console.log(event);
+    event.target.style.backgroundColor = 'green';
+
+});
+
+
+
+
+
+window.addEventListener("resize", () => {
+    let header = document.querySelector('.main-navigation');
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    header.style.background = `#${randomColor}`;
+
+});
+
+window.addEventListener('scroll', function(e) {
+    const random = e.timeStamp % 256 * Math.random();
+    document.body.style.background = `rgba(${random},${random},${random},0.3`;
+    console.log(e.timeStamp % 256 * Math.random())
+  })
+
+  const funLogo = document.querySelector('.intro img');
+
+funLogo.addEventListener('click', function(e) {
+  event.stopPropagation();
+  console.log('bus click');
+
+})
